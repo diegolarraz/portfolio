597b7bea3b9da4d5015d7d50a77c4c12b18eb59b
diegolarraz <diegolarraz1@gmail.com>
2020-07-07 13:03:58 +0200

    added contact links, icons and pdf download feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'download_pdf', to: "pages#download_pdf"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
